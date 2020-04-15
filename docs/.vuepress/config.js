7b83be922c2416b0ddd3b83794bc53a55c4d9e90
Alexander Schwartzberg <aeksco@gmail.com>
2020-04-14 22:38:46 -0400

    Cleaned up docs, moved around some of the structure to better align with the projects current state

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,59 +1,72 @@
 module.exports = {
-  title: 'Codotype',
-  description: 'Hand-crafted starter code for the modern web',
+  title: "Codotype",
+  description: "Hand-crafted starter code for the modern web",
   head: [
-    ['link', { rel: 'icon', href: 'https://res.cloudinary.com/codotype/image/upload/v1552257221/codotype-icons/favicon.png' }],
-    ['script', { src: 'https://cdn.jsdelivr.net/gh/codotype/codotype.github.io@master/js/hotjar.js' }]
+    [
+      "link",
+      {
+        rel: "icon",
+        href:
+          "https://res.cloudinary.com/codotype/image/upload/v1552257221/codotype-icons/favicon.png"
+      }
+    ],
+    [
+      "script",
+      {
+        src:
+          "https://cdn.jsdelivr.net/gh/codotype/codotype.github.io@master/js/hotjar.js"
+      }
+    ]
   ],
   plugins: [
     [
-      '@vuepress/google-analytics',
+      "@vuepress/google-analytics",
       {
-        'ga': process.env.GA_TRACKING_ID
+        ga: process.env.GA_TRACKING_ID
       }
     ]
   ],
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/overview/' },
-      { text: 'Twitter', link: 'https://twitter.com/codotype' },
-      { text: 'GitHub', link: 'https://github.com/codotype' },
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/overview/" },
+      { text: "Twitter", link: "https://twitter.com/codotype" },
+      { text: "GitHub", link: "https://github.com/codotype" }
     ],
     sidebar: [
       {
-        title: 'Hello Codotype', // required
+        title: "Hello Codotype", // required
         // path: '/overview/', // which should be a absolute path.
         collapsable: false, // defaults to true
         sidebarDepth: 2, // defaults to 1
         children: [
-          '/overview/',
-          '/overview/connect',
+          "/overview/",
+          "/overview/how_it_works",
+          "/overview/generators",
+          "/overview/open_source",
+          "/overview/connect"
         ]
       },
       {
-        title: 'Core',
+        title: "Core",
         collapsable: false,
         sidebarDepth: 2,
-        children: [
-          '/core/',
-          '/core/project',
-          '/core/schema',
-          '/core/attribute',
-          '/core/relation',
-        ]
+        children: ["/core/runtime", "/core/cli"]
       },
       {
-        title: 'Generator',
+        title: "Generator",
         collapsable: false,
         sidebarDepth: 2,
         children: [
-          '/generator/',
-          '/generator/structure',
-          '/generator/configuration',
-          '/generator/runtime',
+          "/generator/",
+          "/generator/structure",
+          "/generator/configuration",
+          "/core/project",
+          "/core/schema",
+          "/core/attribute",
+          "/core/relation"
         ]
-      },
+      }
     ]
   }
-}
+};
---
+++ docs/overview/how_it_works.md
@@ -0,0 +1,12 @@
+# How It Works
+
+At its core, **Codotype** is just fancy machinery that makes it easier to create files and directories programatically.
+
+<!-- but its structure, opinions, and surounding tools make it much more powerful -->
+
+<!-- The contents of the resulting files and the structure of their surrounding directories is entirely open to configuration - it's not just a tool for any one programming language. -->
+
+**TODO** - document + diagram architecture here, including:
+
+- high-level interplay beween core machinery & abstractions
+- separation of concerns between generator, runtime, cli, ui
